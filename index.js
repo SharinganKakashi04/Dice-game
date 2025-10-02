e71599942b5c340f795b3503b59474ec37012d95
SharinganKakashi04 <sharingankakashi04@github.com>
2025-10-02 22:52:11 +0530

    Follow the comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// change the entirie randomizer logic to make it more efficient
 var rando1 = Math.random();
 var rando2 = Math.random();
 rando1 = rando1*10;
@@ -13,11 +14,13 @@ const images = [
     'images/dice5.png',
     'images/dice6.png'
 ]
+//check these
 var selectedImage1 = images[randomizer1];
 var selectedImage2 = images[randomizer2];
 document.querySelector(".img1").setAttribute("src",selectedImage1);
 document.querySelector(".img2").setAttribute("src",selectedImage2);
 
+//set to a better one
 if(randomizer2 > randomizer1){
     document.querySelector("h1").innerHTML = "Player 2 wins";
 }
